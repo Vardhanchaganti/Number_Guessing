1bf4999134a80fc8b5b16594af0729f099954397
VardhanChaganti <chagantigirirajavardhan@gmail.com>
2025-08-11 15:08:57 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+let random = Math.floor(Math.random()*100)+1;
+
+const guess = document.getElementById("guess");
+const sub = document.getElementById("sub");
+const result = document.getElementById("result");
+
+let guessCount = 0;
+
+let history = document.getElementById("history");
+
+let record = "";
+
+sub.onclick = function(){
+    guessCount++;
+    let userGuess = Number(guess.value);
+    if(userGuess === random){
+        result.textContent = `Yehhh.,Correct...The value you Guessed(${userGuess}) is right...You took ${guessCount} chances to Guess it`;
+        result.style.color = "green";
+        record += result.textContent + "<br>";
+    }else if(userGuess > random){
+        result.textContent = `Incorrect!!!The Value you Guessed(${userGuess}) is bigger than the actual value.`;
+        result.style.color = "red";        
+        record += result.textContent + "<br>";    
+    }else{
+        result.textContent = `Incorrect!!!The Value you Guessed(${userGuess}) is smaller than the actual value.`;
+        result.style.color = "red";
+        record += result.textContent + "<br>";
+    }
+    history.innerHTML = record;
+}
